26393590873ea7e6b575074c522efe0781f572d7
bayudani <bayu22122017@gmail.com>
2023-12-19 21:58:14 +0700

    perbaiki

--- src/main/java/com/finalproject/panda/Controller/UserController.java
+++ src/main/java/com/finalproject/panda/Controller/UserController.java
@@ -1,41 +1,37 @@
 package com.finalproject.panda.Controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.finalproject.panda.Service.UserService;
 import com.finalproject.panda.model.User;
 
 @Controller
-@RequestMapping("/panda")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-        private static final Logger log = LoggerFactory.getLogger(UserService.class);
-
-
+    @GetMapping("/")
+    public String menu(){
+        return "MenuUtama";
+    }
+    
     @GetMapping("/login")
-    public String login() {
+    public String login(){
         return "LoginPage";
     }
-
-    @GetMapping("/pengaduann")
-    public String pengaduann() {
+ @GetMapping("/pengaduann")
+    public String pengaduann(){
         return "PengaduanPage";
     }
-
-    // form new user
+    //form new user
     @GetMapping("/daftar")
-    public String daftarPage(Model model) {
+    public String daftarPage(Model model){
         try {
             User user = new User();
             model.addAttribute("user", user);
@@ -48,30 +44,29 @@ public class UserController {
 
     // save user to db
     @PostMapping("/daftar")
-    public String saveUser(Model model, User users) {
+    public String saveUser(Model model, User users){
         try {
             userService.saveUser(users);
             model.addAttribute("user", users);
         } catch (Exception e) {
-
+            
         }
-        return "redirect:/panda/login";
+        return "redirect:/";
     }
 
-    // login
+    //login
     @PostMapping("/login")
-    public String loginSucces(
-            @RequestParam String nik,
-            @RequestParam String password) {
+    public String loginSucces(@RequestParam String nik, @RequestParam String password){
         User loggedUser = userService.checkLogin(nik, password);
         if (loggedUser != null) {
 
-            log.info(loggedUser.getNama_lengkap() + "user barhasil login ");
-            return "redirect:/panda/pengaduan";
-        } else {
-            return "redirect:/panda/login";
+            return "redirect:/pengaduann";
+        } else {  
+            return "redirect:/login";
 
         }
-    }
+    }  
 
+   
+    
 }
