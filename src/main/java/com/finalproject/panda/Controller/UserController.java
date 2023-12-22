0f35e611cfbeb14e014ba3b12694520ed0037153
Agus <agusptr44@gmail.com>
2023-12-22 15:20:29 +0700

    feth

--- src/main/java/com/finalproject/panda/Controller/UserController.java
+++ src/main/java/com/finalproject/panda/Controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.finalproject.panda.Service.UserService;
 import com.finalproject.panda.model.User;
@@ -48,19 +50,25 @@ public class UserController {
             User user = new User();
             model.addAttribute("user", user);
         } catch (Exception e) {
-            // Handle exceptions
         }
         return "User/DaftarPage";
     }
 
     @PostMapping("/daftar")
-    public String saveUser(Model model, User users) {
+    public String saveUser(Model model, @RequestPart("fotoFile") MultipartFile fotoFile, User users) {
         try {
+            if (fotoFile != null && !fotoFile.isEmpty()) {
+                byte[] fotoBytes = fotoFile.getBytes();
+                users.setFoto(fotoBytes);
+            }
+
             userService.saveUser(users);
             model.addAttribute("user", users);
         } catch (Exception e) {
-            // Handle exceptions
+            // Tangani pengecualian
+            e.printStackTrace();
         }
         return "redirect:/panda/login";
     }
+
 }
